af94db64d7a4afd9b4cf83a4be71047a3383597a
CodeWithInferno <infernotech2618@gmail.com>
2025-12-09 12:49:47 -0500

    Remove broken MDX files with invalid template syntax
    These files had nested code blocks with {{}} template variables
    that MDX parser couldn't handle. Removed to fix build.

--- pages/examples/_meta.js
+++ pages/examples/_meta.js
@@ -1,8 +1,5 @@
 export default {
   "index": "Examples Overview",
-  "academic-research-setup": "Academic Research",
-  "project-management-setup": "Project Management",
-  "template-gallery": "Template Gallery",
   "workspaces": "Workspace Structures",
   "templates-library": "Templates Library",
   "bases-examples": "Bases Examples",
